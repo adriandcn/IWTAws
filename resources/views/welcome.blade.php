227a69e07aef5058a415bb2462799d6d01102848
Adrian <adrian.dcn@hotmail.com>
2020-03-24 21:18:14 -0500

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    iWaNaTrip
                 </div>
 
                 <div class="links">
